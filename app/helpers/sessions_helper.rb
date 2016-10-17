f832b92dddaf3000a33efe4fab689751b8aa01ec
Anh Trung <phananhtrung.140895@gmail.com>
2016-10-17 17:12:03 +0700

    logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,8 @@ module SessionsHelper
 	end
 
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
-	end
+    	@current_user ||= User.find_by(id: session[:user_id])
+   	end
 
 	def logged_in?
 		!current_user.nil?
@@ -14,10 +14,5 @@ module SessionsHelper
 	def log_out
 		session.delete(:user_id)
 		@current_user = nil
-	end
-
-	def destroy
-		log_out
-		redirect_to root_url
 	end
 end
